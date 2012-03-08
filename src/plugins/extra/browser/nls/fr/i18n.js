c9df7f6eacfd512e27a7b6329307c5d10403501c
nka11 <nka11@nka.me>
2012-03-08 18:45:21 +0100

    adding fr nls

---
+++ src/plugins/extra/browser/nls/fr/i18n.js
@@ -0,0 +1,18 @@
+define( {
+	'jgrid.locale' : 'fr',
+	'button.switch-metaview.tooltip': 'Switch between meta and normal view',
+	'Browsing' : 'Navigation',
+	'Searching for' : 'Recherche de',
+	'in' : 'dans',
+	'Viewing' : 'Visualisation de',
+	'numerous' : 'numerous',
+	'of' : 'de',
+	'Close' : 'Fermer',
+	'Open' : 'Ouvrir',
+	'Search' : 'Chercher',
+	'Name' : 'Nom',
+	'URL' : 'URL',
+	'Preview' : 'Preview',
+	'No records to view' : 'Pas d\'enregistrement',
+	'Input search text...' : 'Entrez un texte a chercher...'
+} );
